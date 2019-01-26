a772acb7ec0dcba00b581f1f0630c67cdf8e6a2a
Wilhuff-Tarkin <giantcaterpillar@gmail.com>
2019-01-26 13:13:15 +0100

    dodalem "Krzysiu"

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    private static void sayHello (String name){
+        System.out.println("witaj" + " " +  name);
+    }
+
+    public static void main(String[] args) {
+        sayHello("Krzysiu");
+    }
 }
